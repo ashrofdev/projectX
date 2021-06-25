afc19d8e26c26681cc82e6cac99e2579cfdd0c4e
Ashrof <salmanashraf853@gmail.com>
2021-06-25 15:30:37 +0100

    perfecting account switch functionality

--- src/dashboardcomponents/Idpopup.js
+++ src/dashboardcomponents/Idpopup.js
@@ -1,28 +1,51 @@
 import React, { useEffect, useState } from 'react';
+// import { useHistory } from 'react-router-dom';
 import { UserContext } from '../Components';
 import { firestore } from '../firebase';
+import { getUser } from '../utils';
 
 const Idpopup = ({toggleAccounts, setUser}) => {
     const user = React.useContext(UserContext)
     const [oneAcc, setOneAcc] = useState(true)
     const [accounts, setAccounts] = useState([])
+    // const history = useHistory()
 
     useEffect(()=>{
         const accounts = []
-        firestore.collection('users').doc(user.userId).collection('accounts').get().then(snap=>{
-            if(!snap.empty){
-                setOneAcc(false)
-                snap.forEach(e=>{
-                    accounts.push(e.data())
-                })
-            }
-            setAccounts(accounts)
-        })
+        if(user.parentAccount){
+            firestore.collection('users').doc(user.parentAccount).collection('accounts').get().then(snap=>{
+                if(!snap.empty){
+                    setOneAcc(false)
+                    snap.forEach(e=>{
+                        accounts.push(e.data())
+                    })
+                }
+                setAccounts(accounts)
+            })
+            getUser(user.parentAccount).then(user=>{
+                accounts.push(user)
+            })
+        }else {
+            firestore.collection('users').doc(user.userId).collection('accounts').get().then(snap=>{
+                if(!snap.empty){
+                    setOneAcc(false)
+                    snap.forEach(e=>{
+                        accounts.push(e.data())
+                    })
+                }
+                setAccounts(accounts)
+            })
+        }
 
 
     },[user])
 
-    const switchAccount = () => {
+    const switchAccount = (userId) => {
+        // history.push("/dashboard")
+        toggleAccounts(false)
+        getUser(userId).then(user=>{
+            setUser(user)
+        })
         
     }
 
@@ -34,20 +57,23 @@ const Idpopup = ({toggleAccounts, setUser}) => {
                         <h4>User IDs</h4>
 
                         <div className="close" onClick={()=> toggleAccounts(false)}>
-                            X
+                            &times;
                         </div>
                     </header>
                     <div className="accounts">
-                        <div className="account">
-                            <p>{user.userId}</p>
-                            <p>NGN{user.balance}</p>
-                        </div>
+                        {
+                            !user.parentAccount?
+                            <div onClick={()=> switchAccount(user.userId)} className="account">
+                                <p>{user.userId}</p>
+                                <p>NGN{user.balance}</p>
+                            </div>:null
+                        }
                         {
                             !oneAcc?
                             <div>
                                 {
                                     accounts.map(account=> {
-                                        return <div className="account">
+                                        return <div onClick={()=> switchAccount(account.userId)} className="account">
                                                     <p>{account.userId}</p>
                                                     <p>NGN{account.balance}</p>
                                                 </div>
