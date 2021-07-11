6712642b6cd2ced014ff0f7245781eaa1be55207
Ashrof <salmanashraf853@gmail.com>
2021-07-11 23:25:32 +0100

    building giealogy tree

--- src/dashboardcomponents/Genealogy.js
+++ src/dashboardcomponents/Genealogy.js
@@ -4,8 +4,145 @@ import {Btn} from '../Components'
 const Genealogy = () => {
     return (
         <div className="genealogy">
-            <div style={{height: '20rem'}} className="tree">
+            <div className="treecontainer">
             {/* // tree  */}
+
+                <div className="tree">
+                    <div className="branchlane">
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>1</p>
+                        </div>
+                    </div>
+                    <div className="branchlane" style={{width: '100%'}}>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>2</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>3</p>
+                        </div>
+                    </div>
+                    <div className="branchlane" style={{width: '97%'}}>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>4</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>5</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>6</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>7</p>
+                        </div>
+                    </div>
+                    <div className="branchlane" style={{width: '88%'}}>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>8</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>9</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>10</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>11</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>12</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>13</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>14</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>15</p>
+                        </div>
+                    </div>
+                    <div className="branchlane" style={{width: '85%'}}>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>16</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>17</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>18</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>19</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>20</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>21</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>22</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>23</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>24</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>25</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>26</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>27</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>28</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>29</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>30</p>
+                        </div>
+                        <div className="branch">
+                            <span>.</span>    
+                            <p>31</p>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <div className="search">
